f7ed71a9679d8d491354658c56b7ee8c949a950b
Ayush Sharma <ayush64sharma@gmail.com>
2026-05-22 10:15:39 +0000

    fix: fix realtime package

--- packages/realtime/tsup.config.ts
+++ packages/realtime/tsup.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs"],
-  dts: true,
+  dts: {
+    compilerOptions: {
+      module: "ESNext",
+      moduleResolution: "Bundler",
+    },
+  },
   outDir: "dist",
   clean: true,
   outExtension() {
